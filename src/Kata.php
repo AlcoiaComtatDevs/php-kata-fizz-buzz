0152734c96c8b557e9df5b3bda18ab389095a81a
Tele <mpalacios@sollutia.com>
2017-05-26 16:47:51 +0200

    Refactoring: Cambio de comillas dobles a simples y cambios en los if's

--- src/Kata.php
+++ src/Kata.php
@@ -7,11 +7,13 @@ class Kata
 {
     public function execute($param){
 
-        if( $param%3 == 0 )
-            return "Fizz";
-        elseif ($param%5 == 0)
-            return "Buzz";
-        else
-            return $param;
+        if( $param%3 === 0 ){
+            return 'Fizz';
+        }
+        if ($param%5 === 0) {
+            return 'Buzz';
+        }
+
+        return $param;
     }
 }
