c5c96a423ba7cb6d443fa9753f9b024478079c1e
Tele <mpalacios@sollutia.com>
2017-05-26 16:40:23 +0200

    test pasado de 3 multiplo de 3

--- src/Kata.php
+++ src/Kata.php
@@ -6,6 +6,10 @@ namespace Kata;
 class Kata
 {
     public function execute($param){
-        return $param;
+
+        if( $param%3 == 0 )
+            return "Fizz";
+        else
+            return $param;
     }
 }
