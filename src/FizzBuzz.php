48050c4c61636b5c1119c584b4cca768ee2c39d1
Tele <mpalacios@sollutia.com>
2017-05-26 17:06:26 +0200

    Refactoring: Constants five and three

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -5,16 +5,22 @@ namespace Kata;
 
 class FizzBuzz
 {
+    const DIVISIBLE_BY_THREE = 3;
+
+    const DIVISIBLE_BY_FIVE = 5;
+
     public function getReplacement($param)
     {
         $returnValue = $param;
-        if ($param % 3 === 0) {
+
+
+        if ($param % self::DIVISIBLE_BY_THREE === 0) {
             $returnValue = 'Fizz';
         }
-        if ($param % 5 === 0) {
+        if ($param % self::DIVISIBLE_BY_FIVE === 0) {
         $returnValue = 'Buzz';
-    }
-        if ($param % 5 === 0 &&$param % 3 === 0 ) {
+        }
+        if ($param % self::DIVISIBLE_BY_FIVE === 0 && $param % self::DIVISIBLE_BY_THREE === 0 ) {
             $returnValue = 'FizzBuzz';
         }
         return $returnValue;
