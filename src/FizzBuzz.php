145eb666d80b472830f6e128401d62b66dce6030
Tele <mpalacios@sollutia.com>
2017-05-26 17:22:02 +0200

    Refactoring: crear metodo que evalua si son divisibles por 3 y 5

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -14,15 +14,43 @@ class FizzBuzz
         $returnValue = $param;
 
 
-        if ($param % self::DIVISIBLE_BY_THREE === 0) {
+        if ($this->checkDivisibleByThree($param)) {
             $returnValue = 'Fizz';
         }
-        if ($param % self::DIVISIBLE_BY_FIVE === 0) {
-        $returnValue = 'Buzz';
+
+        if ($this->checkDivisibleByFive($param)) {
+            $returnValue = 'Buzz';
         }
-        if ($param % self::DIVISIBLE_BY_FIVE === 0 && $param % self::DIVISIBLE_BY_THREE === 0 ) {
+        if ($this->checkDivisibleFiveAndThree($param)) {
             $returnValue = 'FizzBuzz';
         }
         return $returnValue;
+    }
+
+    /**
+     * @param $param
+     * @return boolean
+     */
+    public function checkDivisibleByThree($param)
+    {
+        return $param % self::DIVISIBLE_BY_THREE === 0;
+    }
+
+    /**
+     * @param $param
+     * @return string
+     */
+    public function checkDivisibleByFive($param)
+    {
+        return $param % self::DIVISIBLE_BY_FIVE === 0;
+    }
+
+    /**
+     * @param $param
+     * @return string
+     */
+    public function checkDivisibleFiveAndThree($param)
+    {
+        return $param % self::DIVISIBLE_BY_FIVE === 0 && $param % self::DIVISIBLE_BY_THREE === 0;
     }
 }
